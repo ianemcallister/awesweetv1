96fc5859a7a9e7cff82bd96b8db6249d7b162cd6
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2019-05-27 20:55:17 -0700

    Feat: Inventory Controller

--- public/scripts/webapp.js
+++ public/scripts/webapp.js
@@ -1,16 +1,3 @@
-// FIREBASE
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyBrzabAxyTSzTUw8pP1cVzPkOih1a6I2EQ",
-    authDomain: "cnute-b75af.firebaseapp.com",
-    databaseURL: "https://cnute-b75af.firebaseio.com",
-    projectId: "cnute-b75af",
-    storageBucket: "cnute-b75af.appspot.com",
-    messagingSenderId: "941484130837",
-    appId: "1:941484130837:web:8e568f466246a763"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 // MODULE
-var awesweet = angular.module('awesweet', ['ngRoute', 'ngSanitize', 'ui.bootstrap', 'firebase']);
+var awesweet = angular.module('awesweet', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
