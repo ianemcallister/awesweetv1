2f7c9a6323f889d4dc2c932bf827084c306fbabc
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2019-08-05 11:33:55 -0700

    Fix: Login creds, updated

--- public/scripts/controllers/login-controller.js
+++ public/scripts/controllers/login-controller.js
@@ -9,22 +9,27 @@ angular
 /* @ngInject */
 function loginPageController($scope, $log, $http, $location, firebaseService) {
 
-	//define view model variable
+	//	DEFINE VIEW MODEL VARIABLES
 	var vm = this;
+	vm.username = "";
+	vm.password = "";
 
+	//	NOTIFY PROGRESS
 	$log.info('in the login controller');	    //  TODO: TAKE THIS OUT LATER
 
+	//	VIEW MODEL METHODS
 	vm.submit = function(username, password) {
-		
+
 		//	NOTIFY PROGRESS
 		console.log('submitting form', username, password);
 
 		firebaseService.authenticate.email(username, password)
 		.then(function sucess(s) {
+			$log.info(s)
 			//	ON SUCCESSFUL LOGIN REDIRECT DO TEAM MEMBER DASHBOARD
 			$location.path('/team/'+ s.user.uid +'/dashboard');
 			$scope.$apply();
-			$log.info(s)
+			
 		}).catch(function(e) {
 			$log.error(e);
 		});
